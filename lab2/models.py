f107e0241602b66baf1702b556ca8b616905844a
Nikita Arinkin <nikita.arinkin@gmail.com>
2015-04-18 15:13:20 +0300

    Adding user

--- lab2/models.py
+++ lab2/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.core.signing import Signer
 
-class User(models.Model):
-	username = models.CharField(max_length=30)
+class Users(models.Model):
+	login = models.CharField(max_length=30)
 	email = models.CharField(max_length=50)
 	name = models.CharField(max_length=30)
 	phone = models.CharField(max_length=20)
 	password = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.login;
 	
--- lab2/views.py
+++ lab2/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lab2.models import Users
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,11 @@ def register(request):
 	return render(request, 'lab2/register.html', )
 
 def register_post(request):
+	if request.method == "POST":
+		user = Users(request.POST.get("login", ""),
+					request.POST.get("email", ""),
+					request.POST.get("name", ""),
+					request.POST.get("phone", ""),
+					request.POST.get("password",""));
+		return HttpResponse(user);
 	return render(request, 'lab2/register_success.html',)
